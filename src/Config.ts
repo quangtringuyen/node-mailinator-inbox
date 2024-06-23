c852271cc69cf66f029535d325988200769e5299
tringuyen <Thienan1308@>
2024-06-23 19:26:18 +0700

    change new config

--- src/Config.ts
+++ src/Config.ts
@@ -1,13 +1,15 @@
-
-const USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.2 Safari/605.1.15";
+const USER_AGENT =
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.2 Safari/605.1.15";
 
 export default {
-	INDEX_EP: "https://www.mailinator.com",
-	FETCH_EP: "https://www.mailinator.com/fetch_email?msgid=$EMAIL_ID&zone=public",
-	WS_EP: "wss://www.mailinator.com/ws/fetchinbox?zone=public&query=",
-	USER_AGENT,
-	INDEX_HEADERS: {
-		"User-Agent": USER_AGENT,
-		"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-	},
-}
+  INDEX_EP:
+    "https://www.mailinator.com/v4/public/inboxes.jsp?to=thao.origin.0007",
+  FETCH_EP:
+    "https://www.mailinator.com/fetch_email?msgid=$EMAIL_ID&zone=public",
+  WS_EP: "wss://www.mailinator.com/ws/fetchpublic",
+  USER_AGENT,
+  INDEX_HEADERS: {
+    "User-Agent": USER_AGENT,
+    Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+  },
+};
--- src/Inbox.ts
+++ src/Inbox.ts
@@ -1,182 +1,204 @@
 import got from "got";
 import Config from "./Config";
-import {CookieJar} from "tough-cookie";
-import * as WebSocket from "ws"
-import {EmailHeader, MailinatorEmail, MailinatorEmailHeader} from "./Types";
+import { CookieJar } from "tough-cookie";
+import * as WebSocket from "ws";
+import { EmailHeader, MailinatorEmail, MailinatorEmailHeader } from "./Types";
 import Email from "./Email";
-import {sleep} from "./Util";
+import { sleep } from "./Util";
 
 export default class Inbox {
-	public readonly username: string;
-	private readonly cookieJar: CookieJar;
+  public readonly username: string;
+  private readonly cookieJar: CookieJar;
 
-	private websocket: WebSocket;
-	private _emailHeaders: EmailHeader[];
+  private websocket: WebSocket;
+  private _emailHeaders: EmailHeader[];
 
-	private wsHeaderReceived;
-	private settled: boolean;
-	private promiseCallbacks: {
-		resolve: () 			=> void,
-		reject : (err: Error) 	=> void,
-	};
+  private wsHeaderReceived;
+  private settled: boolean;
+  private promiseCallbacks: {
+    resolve: () => void;
+    reject: (err: Error) => void;
+  };
 
-	constructor(username: string) {
-		this.username = username;
-		this.cookieJar = new CookieJar();
-	}
+  constructor(username: string) {
+    this.username = username;
+    this.cookieJar = new CookieJar();
+  }
 
-	public async refresh() {
-		await this.getCookies();
-		await this.loadInbox();
-	}
+  public async refresh() {
+    await this.getCookies();
+    await this.loadInbox();
+  }
 
-	public async waitForEmailsFrom(address: string | RegExp, timeout: number = 15000): Promise<EmailHeader[]> {
-		const startTime = Date.now();
-		const already = this.getEmailsFrom(address);
-		while(Date.now() - startTime < timeout){
-			const emails = this.getEmailsFrom(address);
-			const newEmails = emails.filter(em => already.findIndex(a => a.id === em.id) < 0);
-			if(newEmails.length > 0){
-				return newEmails;
-			}
-			await sleep(1000);
-			await this.refresh();
-		}
-		return null;
-	}
+  public async waitForEmailsFrom(
+    address: string | RegExp,
+    timeout: number = 15000
+  ): Promise<EmailHeader[]> {
+    const startTime = Date.now();
+    const already = this.getEmailsFrom(address);
+    while (Date.now() - startTime < timeout) {
+      const emails = this.getEmailsFrom(address);
+      const newEmails = emails.filter(
+        (em) => already.findIndex((a) => a.id === em.id) < 0
+      );
+      if (newEmails.length > 0) {
+        return newEmails;
+      }
+      await sleep(1000);
+      await this.refresh();
+    }
+    return null;
+  }
 
-	public getEmailsFrom(address: string | RegExp): EmailHeader[] {
-		return this.emailHeaders?.filter(eh => {
-			if(address instanceof RegExp){
-				return address.test(eh.fromEmail);
-			}else{
-				return eh.fromEmail === address;
-			}
-		}) ?? [];
-	}
+  public getEmailsFrom(address: string | RegExp): EmailHeader[] {
+    return (
+      this.emailHeaders?.filter((eh) => {
+        if (address instanceof RegExp) {
+          return address.test(eh.fromEmail);
+        } else {
+          return eh.fromEmail === address;
+        }
+      }) ?? []
+    );
+  }
 
-	public async getEmail(id: string): Promise<Email> {
-		const url = Config.FETCH_EP.replace("$EMAIL_ID", id);
+  public async getEmail(id: string): Promise<Email> {
+    const url = Config.FETCH_EP.replace("$EMAIL_ID", id);
 
-		try{
-			const response = await got.get(url, {
-				headers: {
-					"User-Agent": Config.USER_AGENT
-				},
-				cookieJar: this.cookieJar,
-				responseType: "json"
-			});
+    try {
+      const response = await got.get(url, {
+        headers: {
+          "User-Agent": Config.USER_AGENT,
+        },
+        cookieJar: this.cookieJar,
+        responseType: "json",
+      });
 
-			return new Email(<MailinatorEmail>(<any>response.body).data);
-		}catch (err){
-			throw new Error("Could not retrieve email. Inner error: "+ (err.message ?? err.toString()));
-		}
-	}
+      return new Email(<MailinatorEmail>(<any>response.body).data);
+    } catch (err) {
+      throw new Error(
+        "Could not retrieve email. Inner error: " +
+          (err.message ?? err.toString())
+      );
+    }
+  }
 
-	private async loadInbox(){
-		this.settled = false;
-		this.wsHeaderReceived = false;
-		this._emailHeaders = [];
-		return new Promise<void>((resolve, reject) => {
-			this.promiseCallbacks = {resolve, reject};
-			try{
-				this.createSocket();
-			}catch(err){
-				this.rejectPromise(new Error("Could not create websocket"));
-			}
-		});
-	}
+  private async loadInbox() {
+    this.settled = false;
+    this.wsHeaderReceived = false;
+    this._emailHeaders = [];
+    return new Promise<void>((resolve, reject) => {
+      this.promiseCallbacks = { resolve, reject };
+      try {
+        this.createSocket();
+      } catch (err) {
+        console.log(err);
+        this.rejectPromise(new Error("Could not create websocket"));
+      }
+    });
+  }
 
-	private createSocket() {
-		this.websocket = new WebSocket(`${Config.WS_EP}${this.username}`, {
-			headers: {
-				"User-Agent": Config.USER_AGENT,
-				"Cookie": this.cookieString,
-			}
-		});
+  private createSocket() {
+    this.websocket = new WebSocket(`${Config.WS_EP}`, {
+      headers: {
+        "User-Agent": Config.USER_AGENT,
+        Cookie: this.cookieString,
+      },
+    });
 
-		this.websocket.on("message", this.messageHandler.bind(this));
-		this.websocket.on("error", (err) => {
-			this.rejectPromise(err);
-		});
-		this.websocket.on("close", (code, reason) => {
-			this.rejectPromise(new Error(`Websocket closed unexpectedly. Code=${code} ; Reason='${reason}'`));
-		});
-	}
+    this.websocket.on("message", this.messageHandler.bind(this));
+    this.websocket.on("error", (err) => {
+      this.rejectPromise(err);
+    });
+    this.websocket.on("close", (code, reason) => {
+      this.rejectPromise(
+        new Error(
+          `Websocket closed unexpectedly. Code=${code} ; Reason='${reason}'`
+        )
+      );
+    });
+    this.websocket.on("open", () => {
+      this.websocket.send(
+        JSON.stringify({ cmd: "sub", channel: this.username })
+      );
+    });
+  }
 
-	private rejectPromise(error: Error) {
-		if(!this.settled) {
-			this.settled = true;
-			console.log("Before delete",this,this.emailHeaders);
-			this._emailHeaders = null;
-			if(this.websocket != null) {
-				this.websocket.close();
-				this.websocket = null;
-			}
-			this.promiseCallbacks.reject(error);
-			delete this.promiseCallbacks;
-		}
-	}
+  private rejectPromise(error: Error) {
+    if (!this.settled) {
+      this.settled = true;
+      console.log("Before delete", this, this.emailHeaders);
+      this._emailHeaders = null;
+      if (this.websocket != null) {
+        this.websocket.close();
+        this.websocket = null;
+      }
+      this.promiseCallbacks.reject(error);
+      delete this.promiseCallbacks;
+    }
+  }
 
-	private resolvePromise() {
-		if(!this.settled) {
-			this.settled = true;
-			if(this.websocket != null) {
-				this.websocket.close();
-				this.websocket = null;
-			}
-			this.promiseCallbacks.resolve();
-			delete this.promiseCallbacks;
-		}
-	}
+  private resolvePromise() {
+    if (!this.settled) {
+      this.settled = true;
+      if (this.websocket != null) {
+        this.websocket.close();
+        this.websocket = null;
+      }
+      this.promiseCallbacks.resolve();
+      delete this.promiseCallbacks;
+    }
+  }
 
-	private messageHandler(data: string) {
-		const msg = JSON.parse(data);
+  private messageHandler(data: string) {
+    const jsonData = JSON.parse(data);
+    if (jsonData.msgs) {
+      for (let msg of jsonData.msgs) {
+        this.addEmailHeader(msg);
+      }
+      this.resolvePromise();
+    } else if (jsonData.ping) {
+      if (this.wsHeaderReceived) {
+        this.resolvePromise();
+      } else {
+        this.wsHeaderReceived = true;
+      }
+    }
+  }
 
-		if("fromfull" in msg){
-			this.addEmailHeader(msg);
-		}else if("original_query" in msg){
-			if(this.wsHeaderReceived) {
-				this.resolvePromise();
-			}else{
-				this.wsHeaderReceived = true;
-			}
-		}
-	}
+  private addEmailHeader(mailinatorHeader: MailinatorEmailHeader | any) {
+    this._emailHeaders.push({
+      id: mailinatorHeader.id,
+      fromEmail: mailinatorHeader.fromfull,
+      fromName: mailinatorHeader.from,
+      time: mailinatorHeader.time,
+      subject: mailinatorHeader.subject,
+      mailinatorHeader,
+    });
+  }
 
-	private addEmailHeader(mailinatorHeader: MailinatorEmailHeader) {
-		this._emailHeaders.push({
-			id: mailinatorHeader.id,
-			fromEmail: mailinatorHeader.fromfull,
-			fromName: mailinatorHeader.from,
-			time: mailinatorHeader.time,
-			subject: mailinatorHeader.subject,
-			mailinatorHeader,
-		})
-	}
+  private async getCookies() {
+    const resp = await got.get(Config.INDEX_EP, {
+      headers: Config.INDEX_HEADERS,
+      cookieJar: this.cookieJar,
+    });
 
-	private async getCookies(){
-		const resp = await got.get(Config.INDEX_EP, {
-			headers: Config.INDEX_HEADERS,
-			cookieJar: this.cookieJar
-		});
+    if (!this.hasCookie()) {
+      throw new Error("Index request returned no cookie");
+    }
+  }
 
-		if(!this.hasCookie()) {
-			throw new Error("Index request returned no cookie");
-		}
-	}
+  private hasCookie(): boolean {
+    return this.cookieJar
+      .getCookiesSync(Config.INDEX_EP)
+      .some((c) => c.key === "JSESSIONID");
+  }
 
-	private hasCookie(): boolean {
-		return this.cookieJar
-			.getCookiesSync(Config.INDEX_EP)
-			.some(c => c.key==="JSESSIONID");
-	}
+  public get emailHeaders(): EmailHeader[] {
+    return this._emailHeaders;
+  }
 
-	public get emailHeaders(): EmailHeader[] {
-		return this._emailHeaders;
-	}
-
-	private get cookieString(): string {
-		return this.cookieJar.getCookieStringSync(Config.INDEX_EP);
-	}
+  private get cookieString(): string {
+    return this.cookieJar.getCookieStringSync(Config.INDEX_EP);
+  }
 }
